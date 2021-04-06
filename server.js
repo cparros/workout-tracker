6bf579baa508b245fb50cbfc11fed93e365ad0a2
cparros <celparros@gmail.com>
2021-04-06 12:31:10 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.static("public"))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
