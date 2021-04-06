cafff13f1930cc60c9b649f398e64f414160c1fb
cparros <celparros@gmail.com>
2021-04-06 13:21:27 -0400

    added models folder that has workouts db and index js for export for seed data population

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,12 +1,11 @@
-let mongoose = require("mongoose");
-let db = require("../models");
+const mongoose = require("mongoose");
+const db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
 });
 
-let workoutSeed = [
+const workoutSeed = [
   {
     day: new Date().setDate(new Date().getDate()-10),
     exercises: [
