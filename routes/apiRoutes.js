c8a65cf965d1cce1c160f3af47cc0c2df099d49d
cparros <celparros@gmail.com>
2021-04-06 14:07:04 -0400

    look back into api routtes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,22 +2,3 @@ const router = require("express").Router()
 const db = require('../models')
 const Workout = require('../models/workouts');
 
-router.post('/api/workout', ({body}, res) => {
-  Workout.create(body).then(dbWorkout => {
-    res.json(dbWorkout)
-  })
-  .catch(err => {
-    res.status(404).json(err)
-  })
-})
-
-router.post('/api/workout/bulk', ({body}, res) => {
-  Workout.insertMany(body).then(dbWorkout => {
-    res.json(dbWorkout)
-  })
-  .catch(err=> {
-    res.status(404).json(err)
-  })
-})
-
-router.get('/api/workout/:id')
