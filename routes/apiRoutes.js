f6d65d4d46536905d0f3ea04f8140c34c553c951
cparros <celparros@gmail.com>
2021-04-07 09:50:50 -0400

    added api routes to get exercises

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,52 @@
-const router = require("express").Router()
-const db = require('../models')
-const Workout = require('../models/workouts');
+const Workout = require("../models/workouts")
+
+module.exports = function(app){
+  app.get('/api/workouts,', function(req,res){
+    Workout.find()
+    .then(data => {
+      res.json(data)
+    })
+    .catch(err => {
+      res.json(err)
+    })
+  });
+  app.post("/api/workouts",function (req,res){    
+    Workout.create({})
+    .then(data => res.json(data))
+    .catch(err => { 
+        res.json(err)
+    })
+});
+
+  app.get("/api/workouts/range",function(req,res){  
+      Workout.find()
+      .then(data =>{  
+          res.json(data)
+      })
+      .catch(err => { 
+          res.json(err)
+      })
+  });
+
+
+  app.post("/api/workouts/range",function (req,res){    
+      Workout.create({})
+      .then(data => res.json(data))
+      .catch(err => { 
+          res.json(err)
+      })
+  });
+
+  app.put("/api/workouts/:id",({body,params},res)=>{   
+      Workout.findByIdAndUpdate(  
+      params.id,
+      {$push:{exercises:body} },
+      {new: true,runValidators:true }
+      )
+      .then(data => res.json(data))
+      .catch(err => { 
+          res.json(err)
+      })
+  });
+}
 
