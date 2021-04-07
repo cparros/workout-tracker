d9c996d6dc0862297280aa664418911fa9b5dcce
cparros <celparros@gmail.com>
2021-04-07 09:47:20 -0400

    added rewuire for html/api routes and morgan

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 4000;
+const morgan = require('morgan')
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
+app.use(morgan('dev'))
 
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify:false
 });
 
-app.use(require("./routes/htmlRoutes"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
